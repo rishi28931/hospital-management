4333dafceee4a093816dc017ee4cf34366673524
Vishal256 <33131498+Vishal256@users.noreply.github.com>
2017-10-29 19:49:01 +0530

    Add files via upload

---
+++ addoc.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+
+<html>
+<head>
+<title>
+ADD DOCTOR PAGE
+</title>
+</head>
+<body>
+<link rel="stylesheet" type="text/css" href="addoc.css"/>
+<div id="outer">
+<div id="l">
+<br>DOCTOR ID:</br>
+<br>NAME:</br>
+<br>E-MAIL:</br>
+<br>CONTACT NUMBER:</br>
+<br>SPECIALISATION:</br>
+<br>PASSWORD:</br>
+</div>
+<div id="r">
+<form action="" method="POST">
+<br><input type="alphanumeric"/></br>
+<br><input type="text"/></br>
+<br><input type="alphanumeric"/></br>
+<br><input type="numeric"/></br>
+<br><input type="text"/></br>
+<br><input type="password"/></br>
+<br><input type="button" value="submit"/><br>
+</form>
+</div>
+</div>
+</body>
+</html>
---
+++ addpat.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+
+<html>
+<head>
+<title>
+ADD PATIENT PAGE
+</title>
+</head>
+<link rel="stylesheet" type="text/css" href="addpat.css"/>
+<body>
+
+<div id="outer">
+<h1>ADD PATIENT</h1>
+<div id="l">
+<br>PATIENT ID:</br>
+<br>NAME:</br>
+<br>ADDRESS:</br>
+<br>CONTACT NUMBER:</br>
+</div>
+<div id="r">
+<form action="" method="POST">
+<br><input type="alphanumeric"/></br>
+<br><input type="text"/></br>
+<br><input type="alphanumeric"/></br>
+<br><input type="numeric"/></br>
+<br><input type="button" value="submit"/><br>
+</form>
+</div>
+</div>
+</body>
+</html>
